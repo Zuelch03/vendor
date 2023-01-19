8cb40d8deb629a5682fcac9152fc3f7d85e0227b
Greg Zuelch <gregzuelch316@gmail.com>
2023-01-18 21:45:38 -0800

    3

--- src/services/ContactService.js
+++ src/services/ContactService.js
@@ -33,4 +33,9 @@ export class ContactService {
     let dataURL = `${this.serverURL}/contacts/${contactId}`
     return axios.put(dataURL, contact)
   }
+
+  static deleteContact(contactId) {
+    let dataUrl = `${this.serverURL}/contacts/${contactId}`
+    return axios.delete(dataUrl)
+  }
 }
