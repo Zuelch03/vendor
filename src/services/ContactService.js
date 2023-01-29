a2aed7ed669d5651da87ba7a722c9fece1468945
Greg Zuelch <gregzuelch316@gmail.com>
2023-01-29 13:40:29 -0800

    commit

--- src/services/ContactService.js
+++ src/services/ContactService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export class ContactService {
-  static serverURL = `http://localhost:5000`
+  static serverURL = `http://165.232.132.81/api`
 
   static getGroups() {
     let dataURL = `${this.serverURL}/groups`
